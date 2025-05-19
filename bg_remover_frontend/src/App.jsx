e3081e7e856709c624aa73cc9ef8128410796ada
Suyash Baoney <suyashbaoney58@gmail.com>
2025-05-19 22:19:34 +0530

    configured tailwind

--- bg_remover_frontend/src/App.jsx
+++ bg_remover_frontend/src/App.jsx
@@ -1,6 +1,16 @@
 const App = () => {
   return (
-    <div> Hello World</div>
+    <div className="text-3xl font-bold underline"> Hello World </div>
+
+    // <div>
+    //   <h1 className = "text-3xl font-bold underline"> Hello World </h1>
+
+      
+    // </div>
+    // <div className="p-6 space-y-6 text-lg">
+    //   <p className="underline text-blue-600">Basic Underline</p>
+    // </div>
+
   )
 }
 
