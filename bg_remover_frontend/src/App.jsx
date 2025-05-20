3ac9201c160789a5388c960e39ca115ba3054ddb
Suyash Baoney <suyashbaoney58@gmail.com>
2025-05-20 10:47:02 +0530

    Added dependency lucide-react

--- bg_remover_frontend/src/App.jsx
+++ bg_remover_frontend/src/App.jsx
@@ -1,17 +1,9 @@
+
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline"> Hello World </div>
-
-    // <div>
-    //   <h1 className = "text-3xl font-bold underline"> Hello World </h1>
-
-      
-    // </div>
-    // <div className="p-6 space-y-6 text-lg">
-    //   <p className="underline text-blue-600">Basic Underline</p>
-    // </div>
-
+    <div>
+    </div>
   )
 }
 
-export default App;
+export default App; 
