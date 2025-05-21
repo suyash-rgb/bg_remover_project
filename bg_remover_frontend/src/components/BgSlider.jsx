c10ee1574e721416476129a3833f2cb6455af320
Suyash Baoney <suyashbaoney58@gmail.com>
2025-05-21 17:51:05 +0530

    fixes for BgSlider component
    - Fixed key={{category}} → key={category} for proper rendering.
    
    - Changed md:text-size-4xl → md:text-4xl (Tailwind doesn’t support text-size-4xl) in title section.
    
    - Converted sliderPosition to a number using Number(e.target.value) for reliable calculations.
    
    - Updated url("../assets/slide_icon.png") → url("../src/assets/slide_icon.png") for proper asset resolution.
    
    - Changed absolute-top-1/2 → absolute top-1/2 (invalid Tailwind utility).
    
    - Created a dedicated CSS file inside components/BgSlider : BgSlider.css for better modularity

--- bg_remover_frontend/src/components/BgSlider.jsx
+++ bg_remover_frontend/src/components/BgSlider.jsx
@@ -1,6 +1,8 @@
+import './BgSlider.css';
 import React from 'react';
 import { useState } from 'react';
 import { assets, categories } from '../assets/assets';
+
 
 const BgSlider = () => {
 
@@ -8,13 +10,13 @@ const BgSlider = () => {
     const[activeCategory, setActiveCategory] = useState("People");
 
     const handleSliderChange = (e) => {
-        setSliderPosition(e.target.value);
+        setSliderPosition(Number(e.target.value));
     }
 
   return (
     <div className="mb-16 relative">
         {/* Section title */}
-        <h2 className="text-3xl md:text-size-4xl font-bold text-gray-900 mb-12 text-center">
+        <h2 className="text-3xl md:text-4xl font-bold text-gray-900 mb-12 text-center">
             Stunning Quality
         </h2>
 
@@ -22,7 +24,7 @@ const BgSlider = () => {
         <div className="flex justify-center mb-10 flex-wrap">
            <div className="inline-flex gap-4 bg-gray-100 p-2 rounded-full flex-wrap justify-center">
                 {categories.map((category) => (
-                    <button key={{category}} 
+                    <button key={category} 
                             className={`px-6 py-2 rounded-full font-medium ${activeCategory === category? 'bg-white text-gray-800 shadow-sm' : 'text-gray-600 hover:bg-gray-200'} `}
                                 onClick={()=> setActiveCategory(category)}>
                         {category}
