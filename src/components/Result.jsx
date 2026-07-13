da17857a75590b0bb1deef06457b1578a52683f1
Suyash Baoney <suyashbaoney58@gmail.com>
2026-07-13 19:29:50 +0530

    useNavigate for Try Another Image button

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,34 +1,38 @@
 import { useContext } from "react"
 import { AppContext } from "../context/AppContext"
+import { useNavigate } from "react-router-dom";
 
 const Result = () => {
-    const { image, resultImage } = useContext(AppContext)
+    const { image, resultImage } = useContext(AppContext);
+    const navigate = useNavigate();
     return (
         <div className="mx-4 my-3 lg:mx-44 mt-14 min-h-[75wh]">
 
-            {/*image container */}
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="bg-white rounded-lg ox-8 py-6 shadow-md">
+                {/*image container */}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
 
-                {/*left side */}
-                <div className="flex flex-col">
-                    <p className="font-semibold text-gray-600 mb-2">Original</p>
-                    <img src={image ? URL.createObjectURL(image) : ""} alt="" className="rounded-md w-full object-cover" />
-                </div>
+                    {/*left side */}
+                    <div className="flex flex-col">
+                        <p className="font-semibold text-gray-600 mb-2">Original</p>
+                        <img src={image ? URL.createObjectURL(image) : ""} alt="" className="rounded-md w-full object-cover" />
+                    </div>
 
-                {/*right side */}
-                <div className="flex flex-col">
-                    <p className="font-semibold text-gray-600 mb-2">
-                        Background Removed
-                    </p>
-                    <div className="rounded-md border border-gray-300 h-full bg-layer relative overflow-hidden">
-                        <img src={resultImage ? resultImage : ""} alt="" className="w-full object-cover" />
-                        {!resultImage && image && (
-                            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                                <div className="border-4 border-indigo-600 rounded-full h-12 w-12 border-t-transparent animate-spin ">
+                    {/*right side */}
+                    <div className="flex flex-col">
+                        <p className="font-semibold text-gray-600 mb-2">
+                            Background Removed
+                        </p>
+                        <div className="rounded-md border border-gray-300 h-full bg-layer relative overflow-hidden">
+                            <img src={resultImage ? resultImage : ""} alt="" className="w-full object-cover" />
+                            {!resultImage && image && (
+                                <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                                    <div className="border-4 border-indigo-600 rounded-full h-12 w-12 border-t-transparent animate-spin ">
 
+                                    </div>
                                 </div>
-                            </div>
-                        )}
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
@@ -37,7 +41,9 @@ const Result = () => {
             {/*Buttons */}
             {resultImage && (
                 <div className="flex justify-center sm:justify-end items-center flex-wrap gap-4 mt-6">
-                    <button className="border text-indigo-600 font-semibold py-2 px-4 rounded-full text-lg hover:scale-105 transition-all duration-300">
+                    <button
+                        className="border text-indigo-600 font-semibold py-2 px-4 rounded-full text-lg hover:scale-105 transition-all duration-300"
+                        onClick={() => navigate("/")}>
                         Try another image
                     </button>
 
